268cd848ecca1103c0cd711dfa4e526eb99864e4
billy1108 <billy.11080@gmail.com>
2014-01-23 00:16:58 -0500

    Puliendo  jugabilidad con el teclado
    agragando el default y la tecla enter

--- GameBonba/main.cpp
+++ GameBonba/main.cpp
@@ -9,28 +9,32 @@ int main()
     initwindow(700,700);
     Tanque one(300,300);
     Tanque two(600,300);
-    cout<<one.getX()<<endl<<endl;
-    int tecla,jugadas=0;
+    one.pintarTanque(1);two.pintarTanque(1);//pinta
+    int tecla,jugadas=1;
     while(1){
         if(kbhit()){
-            if(jugadas<10){//jugador uno
+            if(jugadas==1){//jugador uno
                 one.pintarTanque(2);two.pintarTanque(2);//borra
                 tecla = getch();
                 cout<<tecla<<endl;
                 switch(tecla){
                 case 77://derecha
-                    cout<<"VALOR ORIGINAL : "<<one.getX()<<endl;
                     one.setX(7);
-                    cout<<"VALOR INCREMENTADO : "<<one.getX()<<endl;
                     one.pintarTanque(1);two.pintarTanque(1);//pinta
                     break;
                 case 75://izquierda
                     one.setX(-7);
                     one.pintarTanque(1);two.pintarTanque(1);//pinta
                     break;
+                case 13:
+                    jugadas=2;
+                    one.pintarTanque(1);two.pintarTanque(1);//pinta
+                    break;
+                default:
+                    one.pintarTanque(1);two.pintarTanque(1);//pinta
+                    break;
                 }
-                jugadas++;
-            }else if(jugadas<20){//jugados dos
+            }else if(jugadas==2){//jugados dos
                 one.pintarTanque(2);two.pintarTanque(2);//borra
                 tecla = getch();
                 switch(tecla){
@@ -42,11 +46,14 @@ int main()
                     two.setX(-7);
                     one.pintarTanque(1);two.pintarTanque(1);//pinta
                     break;
+                case 13:
+                    jugadas=1;
+                    one.pintarTanque(1);two.pintarTanque(1);//pinta
+                    break;
+                default:
+                    one.pintarTanque(1);two.pintarTanque(1);//pinta
+                    break;
                 }
-                jugadas++;
-            }else{
-                one.pintarTanque(1);two.pintarTanque(1);//pinta
-                jugadas=0;
             }
         }
     }
